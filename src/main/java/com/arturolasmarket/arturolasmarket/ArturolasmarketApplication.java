91cbddd5ac93aadc5c31b7e89e651928fcd163dc
Arturo Thinkpad <arturolas@hotmail.com>
2024-02-03 21:53:44 +0100

    Commit inicial.

---
+++ src/main/java/com/arturolasmarket/arturolasmarket/ArturolasmarketApplication.java
@@ -0,0 +1,13 @@
+package com.arturolasmarket.arturolasmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArturolasmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArturolasmarketApplication.class, args);
+	}
+
+}
